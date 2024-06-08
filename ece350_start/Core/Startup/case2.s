443162334841f62a0dddcdea1612d0008d9474c4
Jakob Untch <jakob.untch@gmail.com>
2024-06-08 12:21:23 -0400

    case 2 file

---
+++ ece350_start/Core/Startup/case2.s
@@ -0,0 +1,16 @@
+ .syntax unified
+ .cpu cortex-m4
+ .fpu softvfp
+ .thumb
+
+ .global Case2
+
+ .thumb_func
+
+ Case2:
+
+	MRS R0, PSP
+	LDMIA R0!, {R4, R11}
+	MSR PSP, R0
+	MOV LR, 0xFFFFFFFD
+	BX LR
