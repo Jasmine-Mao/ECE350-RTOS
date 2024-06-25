ae2ee7adef50c59c510c64dd40a4380e72d29a2d
Jasmine Mao <j54mao@uwaterloo.ca>
2024-06-25 09:00:08 -0400

    added new file for k mem functions

---
+++ ece350_start/Core/Src/k_mem.c
@@ -0,0 +1,20 @@
+#include "common.h"
+#include "k_mem.h"
+#include "main.h"
+#include "stm32f4xx_it.h"
+
+int k_mem_init(){
+
+}
+
+void *k_mem_alloc(size_t size){
+
+}
+
+int k_mem_dealloc(void *ptr){
+
+}
+
+int k_mem_count_extfrag(size_t size){
+    
+}
