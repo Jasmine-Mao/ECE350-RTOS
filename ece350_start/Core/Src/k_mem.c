7ed1575c5ebfbf3b958da809d6a0abb055f17519
Ameya Gawde <ameyagawde29@gmail.com>
2024-06-25 09:29:26 -0400

    alloc

--- ece350_start/Core/Src/k_mem.c
+++ ece350_start/Core/Src/k_mem.c
@@ -6,6 +6,7 @@
 
 extern U32 _img_end; //the start of the heap will be from here, could add 0x200 bytes offset
 extern U32 _estack;
+extern U32 _Min_Stack_Size;
 
 
 int k_mem_init(){
@@ -18,13 +19,19 @@ int k_mem_init(){
         k_mem_initialized = 1;
         //initialize the heap
         U32 *heap_start = &_img_end; //set the start of the heap to the end of the image
-        U32 *heap_end = &_estack - 0x4000; //set the end of the heap to _estack - _Min_Stack_Size
+        U32 *heap_end = &_estack - &_Min_Stack_Size; //set the end of the heap to the end of the stack
 
         return RTX_OK;
     }
 }
 
 void *k_mem_alloc(size_t size){
+    //allocates size bytes of memory according to the Buddy System algorithm and returns a pointer to the start of the usable memory in the block
+
+    if (k_mem_initialized == 0 || size == 0){
+        return NULL;
+    }
+
 
 }
 
