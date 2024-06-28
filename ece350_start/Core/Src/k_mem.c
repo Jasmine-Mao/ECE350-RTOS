615f70e0ec724c7dfb1574f66a25f94d4efd55c7
Jasmine Mao <j54mao@uwaterloo.ca>
2024-06-27 22:22:28 -0400

    roughed out the allocate function, needs testing and other eyes to look at it

--- ece350_start/Core/Src/k_mem.c
+++ ece350_start/Core/Src/k_mem.c
@@ -18,18 +18,19 @@ int k_mem_init(){
     }
     else{
         k_mem_initialized = 1;
-
-        header_block *root_block;       // create the very first free block encompassing all the free memory
-        root_block->next = NULL;
-        root_block->ownership = -1;     // negative since there is no current ownership of the block
-        root_block->size = (1 << MAX_ORDER);
-        root_block->status = 0;
-
-        header_array[MAX_ORDER - 1] = root_block;       // place the root memory block at the last index of the pointer array
-        //initialize the heap
         heap_start = &_img_end; //set the start of the heap to the end of the image
         heap_end = &_estack - &_Min_Stack_Size; //set the end of the heap to _estack - _Min_Stack_Size
 
+        header_block *root_block;       // create the very first free block encompassing all the free memory
+        root_block->next = NULL;
+        // root_block->ownership = -1;     // negative since there is no current ownership of the block
+        root_block->size = (1 << MAX_ORDER);        // *** NEED TO CHECK THIS ***
+        root_block->status = 0;
+        root_block->address = heap_start;
+
+        header_array[MAX_ORDER - 1] = root_block;       // place the root memory block at the last index of the pointer array
+        //initialize the heap
+        
         return RTX_OK;
     }
 }
@@ -43,24 +44,70 @@ void *k_mem_alloc(size_t size){
     else{
         // check what order the size fits within
         size += sizeof(header_block);   // add the header to the size
-        int block_order = -1;
+        int block_level = -1;
         for(int i = MIN_BLOCK_ORDER; i <= MIN_BLOCK_ORDER + MAX_ORDER; i++){       // find what memory block size is needed to accomodate
             if((1 << i) < size){
-                block_order = i - MIN_BLOCK_ORDER;      // block order is now the index value needed to find blocks of the size we need
+                block_level = i - MIN_BLOCK_ORDER;      // block order is now the index value needed to find blocks of the size we need
                 break;
             }
         }
-        if(block_order == -1){
+        if(block_level == -1){
             // we didn't find an appropriate size :(
             printf("couldn't fund sufficient block size. exiting...\n");
             return NULL;
         }
-        // check the header array at index block_order. this will be the list of free blocks or the desired size
-        
+        // check the header array at index block_level. this will be the list of free blocks or the desired size
+
+        // if no node is found, the pointer in the array will be NULL. thus, we need to check the pointer value
+        int tracker = block_level;
+        while(header_array[tracker] == NULL){
+            if(tracker == MIN_BLOCK_ORDER + MAX_ORDER){  // if we have reached the root and the pointer is NULL, there is simply no space for our current block
+                return NULL;
+            }
+            tracker++;      // increment tracker to look at the level above
+        }
+        // reach here once we find a list that has a free node we can split
+
+        // now we need to split the free block until we reach our desired block size, block_level
+        while(tracker != block_level){  // stay here until we have reached the block size we want
+            // create 2 nodes in the level below
+            header_block *buddy1;
+            header_block *buddy2;
+
+            buddy1->size = (1 << (tracker + MIN_BLOCK_ORDER));
+            buddy2->size = buddy1->size;
+
+            buddy1->address = header_array[tracker]->address;
+            buddy2->address = buddy1->address + buddy1->size;       // this is pointer arithmetic? need this checked
+
+            buddy1->status = 0;
+            buddy2->status = 0;
+
+            buddy1->next = buddy2;
+            buddy2->next = NULL;
+
+            // remove the now-used node from the free list
+            header_array[tracker]->status = 1;      // set the status to 1 now that it is partially occupied
+            header_array[tracker] = header_array[tracker]->next;    // the pointer of the array now points to the next item in the free list.
+
+            // decrement tracker
+            tracker--;
+            // set the next free list
+            header_array[tracker]->next = buddy1;
+        }
+
+        // now we have a free block we can manipulate
+
+        // also how tf do we assign ownership??
+
+        header_array[block_level]->status = 1;
+        void *pointer = header_array[block_level]->address;
+        header_array[block_level] = header_array[block_level]->next;
+
+        return pointer;
     }
-
-
 }
+
 
 int k_mem_dealloc(void *ptr){
     if(k_mem_initialized == 0){
