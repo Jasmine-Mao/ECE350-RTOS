b2510c4e0ef7758e46112994805a4b72c1d40618
Jasmine Mao <j54mao@uwaterloo.ca>
2024-06-28 11:47:00 -0400

    forgot to push changes to common.h

--- ece350_start/Core/Inc/common.h
+++ ece350_start/Core/Inc/common.h
@@ -49,7 +49,8 @@ typedef struct task_control_block {
 typedef struct header {
 	U32 status;					// free/used, free = 0, used = 1
 	U32 size;					// size of the total block of memory
-	int ownership;				// who owns this block of memory ie. which thread
+	// int ownership;				// who owns this block of memory ie. which thread
+	U32 *address;
 	struct header_block *next;	// next free/occupied block, depending on which list it is for
 } header_block;
 
